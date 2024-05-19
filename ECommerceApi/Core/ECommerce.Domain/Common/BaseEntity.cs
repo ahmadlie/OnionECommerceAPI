def86915de74b7daaf066a1157dde19ea63d0f2b
ahmadlie <eldar.ehmedli2003@mail.ru>
2024-05-19 12:29:58 +0400

    Added First Commit

---
+++ ECommerceApi/Core/ECommerce.Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Common;
+public class BaseEntity : IBaseEntity
+{
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; } = false;
+
+    public DateTime CreatedDate = DateTime.Now;
+    public DateTime UpdatedDate { get; set; }   
+}
